200ba2f228f0a2387087853871c23d1394270e10
harindu-pe <harindu.pe@gmail.com>
2025-01-11 12:54:34 +0530

    tailwind css setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,28 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        circularWeb: ["var(--font-circularWeb)", "sans-serif"],
+        general: ["var(--font-general)", "sans-serif"],
+        robertMedium: ["var(--font-robert-medium)", "sans-serif"],
+        robertRegular: ["var(--font-robert-regular)", "sans-serif"],
+        zentry: ["var(--font-zentry-regular)", "sans-serif"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: {
+          50: "#DFDFF0",
+          75: "#dfdff2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        violet: {
+          300: "#5724ff",
+        },
+        yellow: {
+          100: "#8e983f",
+          300: "#edff66",
+        },
       },
     },
   },
